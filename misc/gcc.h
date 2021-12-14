30d64412fc7f0a9ab1bb616ec91e566af814ccbb
StefanoBelli <sa.belli@hotmail.it>
2021-12-14 15:39:16 +0100

    it is unlikely that kprintf buffer gets filled up

--- misc/gcc.h
+++ misc/gcc.h
@@ -3,6 +3,6 @@
 
 #define __mykapi __attribute__((fastcall))
 //#define likely(expr) __builtin_expect(expr, 1)
-//#define unlikely(expr) __builtin_expect(expr, 0)
+#define unlikely(expr) __builtin_expect(expr, 0)
 
 #endif
