3bdf2d4404666703634699a9e5cef6728dfed7f1
StefanoBelli <sa.belli@hotmail.it>
2021-12-28 10:34:04 +0100

    x86_pic_eoi receives an int number, not a IRQ number

--- x86/pic.h
+++ x86/pic.h
@@ -4,7 +4,7 @@
 #include "types.h"
 
 __mykapi void x86_pic_remap();
-__mykapi void x86_pic_eoi(mykt_uint_8 irq);
+__mykapi void x86_pic_eoi(mykt_uint_8 intno);
 __mykapi void x86_pic_set_mask(mykt_uint_8 irq);
 __mykapi void x86_pic_clear_mask(mykt_uint_8 irq);
 
